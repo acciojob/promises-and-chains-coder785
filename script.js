a4b5eb2f15085305ff97bf91e63cfc0993c7832a
coder785 <116183840+coder785@users.noreply.github.com>
2023-03-29 11:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var nameElement=document.querySelector("#name");
+var ageElement=document.querySelector("#age");
+var btnElement=document.querySelctor("#btn");
+function handelSubmit(e){
+	alert("abc");
+
+}
+
+btnElement.addEventListener('click',handelSubmit);
+
+
